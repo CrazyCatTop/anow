394416a8086f02597621fd6c858e4e5cc4562b5c
Cat <daniliukdaniil@gmail.com>
2023-01-15 02:34:14 +0300

    menu update #1 (it's not ready yet)

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,14 @@
+const menu__links = document.querySelectorAll('.header__menu-link');
+
+menu__links.forEach((link) => {
+    link.addEventListener('click', () => {
+        document.querySelector('.header__menu-link_active').classList.remove('header__menu-link_active');
+        link.classList.add('header__menu-link_active');
+    } ,false);
+});
+
+
+
 const skills__values = document.querySelectorAll('.skills__value'),
       skills__scales = document.querySelectorAll('.skills__scale'),
       skills__titles = document.querySelectorAll('.skills__head title');
