7941e2e67184d5d5a134cbe345a669b7e927e9f3
Cat <daniliukdaniil@gmail.com>
2023-01-16 20:51:23 +0300

    menu update

--- src/js/script.js
+++ src/js/script.js
@@ -4,51 +4,40 @@ const menu__links = document.querySelectorAll('.header__menu-link'),
       st = 'scrollTop',
       sh = 'scrollHeight';
 
-function removeActive() {
-    document.querySelector('.header__menu-link_active').classList.remove('header__menu-link_active');
-} 
-
 function setActive(i) {
+    document.querySelector('.header__menu-link_active').classList.remove('header__menu-link_active');
     menu__links[i].classList.add('header__menu-link_active');
 } 
 
 /* menu__links.forEach((link) => {
     link.addEventListener('click', () => {
-        removeActive();
-        link.classList.add('header__menu-link_active');
+        setActive(menu__links.indexOf(link));
     } ,false);
-}); */ 
+}); */
 
 document.addEventListener('scroll', function() {
 
     let percent = (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
 
     if (percent <= 6.5) {
-        removeActive();
         setActive(0);
     }
     if (percent > 6.5) {
-        removeActive();
         setActive(1);
     }
     if (percent > 28.5 && percent < 52) {
-        removeActive();
         setActive(2);
     }
     if (percent > 52 && percent < 79.5) {
-        removeActive();
         setActive(3);
     }
     if (percent > 79.5 && percent < 91.5) {
-        removeActive();
         setActive(4);
     }
     if (percent > 91.5 && percent < 98) {
-        removeActive();
         setActive(5);
     }
     if (percent > 98) {
-        removeActive();
         setActive(6);
     }
 
